1674350e86785ef3a049ea7585622fe72ce03ebb
hl <1813927324@qq.com>
2019-09-03 23:00:02 +0800

    改造validate的调用

--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+import VeeValidate, { Validator } from 'vee-validate'
+// 导入VeeValidate的中文包
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 import 'vant/lib/index.css'
 import './styles/index.css'
 
-Vue.config.productionTip = false
 Vue.use(Vant)
+Vue.use(VeeValidate, {
+  // 文本框中触发验证的事件 默认是input
+  events:''
+})
+// 配置中文
+Validator.localize('zhCN', zhCN);
+Vue.config.productionTip = false
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,8 +4,23 @@
     <van-nav-bar title="标题" />
     <!-- 输入框 -->
     <van-cell-group>
-      <van-field left-icon="phone-o" v-model="user.mobile" placeholder="请输入手机号" />
-      <van-field left-icon="star-o" v-model="user.code" placeholder="请输入验证码">
+      <van-field
+        v-validate="'required|digits:11'"
+        name="mobile"
+        :error-message="errors.first('mobile')"
+        left-icon="phone-o"
+        v-model="user.mobile"
+        placeholder="请输入手机号"
+      />
+
+      <van-field
+        v-validate="'required|digits:6'"
+        name="code"
+        :error-message="errors.first('code')"
+        left-icon="star-o"
+        v-model="user.code"
+        placeholder="请输入验证码"
+      >
         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
       </van-field>
     </van-cell-group>
@@ -17,36 +32,59 @@
 </template>
 
 <script>
-
-import { login } from '../api/user'
-import { mapMutations } from 'vuex'
+import { login } from "../api/user";
+import { mapMutations } from "vuex";
 
 export default {
-  data () {
+  data() {
     return {
       user: {
-        mobile: '',
-        code: ''
+        mobile: "",
+        code: ""
       }
-    }
+    };
+  },
+  created() {
+    // 配置VeeValidate的自定义信息
+    const dict = {
+      custom: {
+        mobile: {
+          required: "请输入手机号码",
+          digits: "手机号码必须是11位数字"
+        },
+        code: {
+          required: "请输入验证码",
+          digits: "验证码必须是6位数字"
+        }
+      }
+    };
+    this.$validator.localize("en", dict);
   },
   methods: {
-    ...mapMutations(['setUser']),
+    ...mapMutations(["setUser"]),
     // 点击按钮  处理登录
-    async handleLogin () {
+    async handleLogin() {
       try {
-        let data = await login(this.user)
-        this.$toast.success('登录成功')
-        this.$router.push('/')
+        // 表单验证
+       const valid = await this.$validator.validate()
+          // 验证失败
+        if (!valid) {
+              return
+        }
+        // data是接口返回中的数据 (因为在响应拦截器做了处理)
+        let data = await login(this.user);
         // 登录成功之把token存放到 store 和本地 存储中
-        //   this.$store.commit('setUser', data)
-        this.setUser(data)
+        // this.$store.commit('setUser', data)
+        this.setUser(data);
+        this.$toast.success("登录成功");
+        this.$router.push("/");
+
       } catch (err) {
-        this.$toast.fail('登录失败')
+        this.$toast.fail("登录失败");
       }
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
